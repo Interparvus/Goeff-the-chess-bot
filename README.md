dd55d7092504c3a492d7a91415d9a4ccdccde543
Interparvus <michal.holan.on@gmail.com>
2022-10-03 23:51:11 +0200

    readme, chromedriver

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Goeff-the-chess-bot
+
+
+Req:
+https://chromedriver.chromium.org/downloads         (Version of chrome driver must be same as the version of your UPDATED chrome browser(this can be checked here : chrome://settings/help )), make sure to change folder path.
+Python 3.9
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from selenium import *
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+import json
+driver = webdriver.Chrome("C:\\Users\\admin\\schecter\\Pracovná plocha\\Goeff the chess bot\\Goeff-the-chess-bot\\chromedriver_win32\\chromedriver.exe")
+url = "https://www.chess.com"
+driver.get(url)
+time.sleep(3)
